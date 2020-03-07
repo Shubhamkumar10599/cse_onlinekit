cf9e47b49137d40c645ece658a277925de1a842f
SAURAV SUMAN <appmaker121@gmail.com>
2020-03-07 12:52:55 +0530

    Initial commit

---
+++ online_study/online_study/views.py
@@ -0,0 +1,8 @@
+from django.contrib.sites import requests
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(requests):
+    params = {'name': 'harry', 'place': 'mars'}
+    return render(requests, 'index.html', params)
---
+++ online_study/statics/js/mainsidebar.js
@@ -0,0 +1,10 @@
+const openNav= () =>{
+    
+    document.getElementById('sidebarMenu').style.width="250px";
+}
+
+const closeNav= () =>{
+    
+    document.getElementById('sidebarMenu').style.width="0";
+    
+}

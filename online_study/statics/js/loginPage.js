fb5e4919c384e5d51256b897a1fa999010d2aeb8
SAURAV SUMAN <appmaker121@gmail.com>
2020-03-16 19:51:40 +0530

    final login page completed

--- online_study/statics/js/loginPage.js
+++ online_study/statics/js/loginPage.js
@@ -1,4 +1,4 @@
-const signUpButton = document.getElementById('signUp');
+const signUpButton = document.getElementById('signUp1');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
@@ -9,3 +9,25 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+
+
+const x=document.getElementById("login");
+const y=document.getElementById("signup");
+const z=document.getElementById("btn");
+
+function register()
+{
+	x.style.left="-400px";
+	y.style.left="100px";
+	z.style.left="112.5px";
+}
+
+
+function login()
+{
+	x.style.left="100px";
+	y.style.left="500px";
+	z.style.left="0px";
+}

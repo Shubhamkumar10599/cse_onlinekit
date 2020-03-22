291391b09995a88c1ff594638e9619239282b234
SAURAV SUMAN <appmaker121@gmail.com>
2020-03-22 22:11:46 +0530

    new content will be added

--- online_study/statics/js/loginPage.js
+++ online_study/statics/js/loginPage.js
@@ -2,6 +2,7 @@ const signUpButton = document.getElementById('signUp1');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
+
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
@@ -16,12 +17,20 @@ signInButton.addEventListener('click', () => {
 const x=document.getElementById("login");
 const y=document.getElementById("signup");
 const z=document.getElementById("btn");
+const changetext = document.getElementById('login1');
+
 
 function register()
 {
 	x.style.left="-400px";
 	y.style.left="100px";
 	z.style.left="112.5px";
+	z.style.color='white';
+		changetext.innerText='CREATE ACCOUNT'
+
+
+
+
 }
 
 
@@ -30,4 +39,8 @@ function login()
 	x.style.left="100px";
 	y.style.left="500px";
 	z.style.left="0px";
+	changetext.innerText='LOGIN'
+		z.style.color='white';
+
+
 }

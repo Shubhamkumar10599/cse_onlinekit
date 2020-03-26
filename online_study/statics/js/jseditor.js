edbb321cbd04887e5645817460893ce0dc57cf00
SAURAV SUMAN <appmaker121@gmail.com>
2020-03-26 18:01:28 +0530

    some more content will be added.

--- online_study/statics/js/jseditor.js
+++ online_study/statics/js/jseditor.js
@@ -9,3 +9,24 @@
     editor.renderer.$cursorLayer.element.style.display = "none"
     editor.renderer.setShowGutter(false);
     document.getElementById('editor').style.fontSize='17px';
+
+
+
+
+    var editor1 = ace.edit("editor1");
+    editor1.setTheme("ace/theme/eclipse");
+    editor1.session.setMode("ace/mode/javascript");
+    editor1.setReadOnly(true);
+    editor1.setHighlightActiveLine(false);
+    editor1.setShowPrintMargin(false);
+    editor1.session.setUseWrapMode(true);
+    editor1.renderer.$cursorLayer.element.style.display = "none"
+    editor1.renderer.setShowGutter(false);
+    document.getElementById('editor1').style.fontSize='17px'
+
+
+
+function alertFunction (){
+	alert('Hi! Buddy this is alert box ');
+}
+

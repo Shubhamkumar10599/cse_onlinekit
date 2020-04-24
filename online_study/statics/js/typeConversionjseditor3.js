fb5e8001a151bca953eeb97858380f3fab4e37b9
SAURAV SUMAN <appmaker121@gmail.com>
2020-04-24 18:39:11 +0530

    Template inheritance is done code will be optimized

---
+++ online_study/statics/js/typeConversionjseditor3.js
@@ -0,0 +1,85 @@
+var editor11 = ace.edit("editor11");
+    editor11.setTheme("ace/theme/eclipse");
+    editor11.session.setMode("ace/mode/javascript");
+    editor11.setReadOnly(true);
+    editor11.setHighlightActiveLine(false);
+    editor11.setShowPrintMargin(false);
+    editor11.session.setUseWrapMode(true);
+    editor11.renderer.$cursorLayer.element.style.display = "none";
+    editor11.renderer.setShowGutter(false);
+    document.getElementById('editor11').style.fontSize='17px';
+
+
+
+
+
+    var editor2 = ace.edit("editor2");
+    editor2.setTheme("ace/theme/eclipse");
+    editor2.session.setMode("ace/mode/javascript");
+    editor2.setReadOnly(true);
+    editor2.setHighlightActiveLine(false);
+    editor2.setShowPrintMargin(false);
+    editor2.session.setUseWrapMode(true);
+    editor2.renderer.$cursorLayer.element.style.display = "none";
+    editor2.renderer.setShowGutter(false);
+    document.getElementById('editor2').style.fontSize='17px';
+
+
+
+
+    var editor3 = ace.edit("editor3");
+    editor3.setTheme("ace/theme/eclipse");
+    editor3.session.setMode("ace/mode/javascript");
+    editor3.setReadOnly(true);
+    editor3.setHighlightActiveLine(false);
+    editor3.setShowPrintMargin(false);
+    editor3.session.setUseWrapMode(true);
+    editor3.renderer.$cursorLayer.element.style.display = "none"
+    editor3.renderer.setShowGutter(false);
+    document.getElementById('editor3').style.fontSize='17px';
+
+
+
+
+
+     var editor4 = ace.edit("editor4");
+    editor4.setTheme("ace/theme/eclipse");
+    editor4.session.setMode("ace/mode/javascript");
+    editor4.setReadOnly(true);
+    editor4.setHighlightActiveLine(false);
+    editor4.setShowPrintMargin(false);
+    editor4.session.setUseWrapMode(true);
+    editor4.renderer.$cursorLayer.element.style.display = "none";
+    editor4.renderer.setShowGutter(false);
+    document.getElementById('editor4').style.fontSize='17px';
+
+
+
+
+
+
+
+    var editor5 = ace.edit("editor5");
+    editor5.setTheme("ace/theme/eclipse");
+    editor5.session.setMode("ace/mode/javascript");
+    editor5.setReadOnly(true);
+    editor5.setHighlightActiveLine(false);
+    editor5.setShowPrintMargin(false);
+    editor5.session.setUseWrapMode(true);
+    editor5.renderer.$cursorLayer.element.style.display = "none";
+    editor5.renderer.setShowGutter(false);
+    document.getElementById('editor5').style.fontSize='17px';
+
+
+
+
+     var  editor6 = ace.edit("editor6");
+    editor6.setTheme("ace/theme/eclipse");
+    editor6.session.setMode("ace/mode/javascript");
+    editor6.setReadOnly(true);
+    editor6.setHighlightActiveLine(false);
+    editor6.setShowPrintMargin(false);
+    editor6.session.setUseWrapMode(true);
+    editor6.renderer.$cursorLayer.element.style.display = "none"
+    editor6.renderer.setShowGutter(false);
+    document.getElementById('editor6').style.fontSize='17px'
